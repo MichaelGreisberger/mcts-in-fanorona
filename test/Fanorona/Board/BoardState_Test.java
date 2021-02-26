d6b5b6e2c6afcf22787f850dd955169d1a5f0363
MichaelGreisberger <mike.greisberger@gmail.com>
2021-02-26 14:38:19 +0100

    commented test cases that where used to quickly evaluate specific game positions

--- test/Fanorona/Board/BoardState_Test.java
+++ test/Fanorona/Board/BoardState_Test.java
@@ -1,16 +1,7 @@
 package Fanorona.Board;
 
-import Fanorona.Game;
-import Fanorona.Move.Move;
-import Fanorona.Player.Player;
-import Fanorona.Player.RandomMctsPlayer;
-import Fanorona.Player.SteeredMaterialPlayer;
-import Fanorona.Player.SteeredMctsPlayer;
-import Fanorona.Util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
 
 class BoardState_Test {
 
@@ -40,47 +31,41 @@ class BoardState_Test {
         Assertions.assertEquals(expectedBlack, state.countPieces(2));
     }
 
-    @Test
-    void twoFiveDrawExample() {
-        Board board = new Board(BoardSize.LARGE, BoardStateExamples.TwoFiveDrawExample_char);
-        Player p1 = new SteeredMaterialPlayer(5000, true, "puct");
-        Player p2 = new SteeredMaterialPlayer(5000, true, "puct");
+//    @Test
+//    void twoFiveDrawExample() {
+//        Board board = new Board(BoardSize.LARGE, BoardStateExamples.TwoFiveDrawExample_char);
+//        Player p1 = new SteeredMaterialPlayer(5000, true, "puct");
+//        Player p2 = new SteeredMaterialPlayer(5000, true, "puct");
+//        Game game = new Game(board, p1, p2);
+//        game.startGame();
+//    }
+
+
+//    @Test
+//    void test() {
+//        RandomMctsPlayer random = new RandomMctsPlayer(1000, true);
+//        SteeredMctsPlayer uct = new SteeredMctsPlayer(1000, true);
 //        try {
-////            p2 = StreamPlayer.initWithSchaddExecutable(5000,System.getProperty("user.dir") + File.separator + Main.schaddFileName, new SchaddsPlayerCommunicationDelegate(), 2, true);
+//            System.out.println(Util.getRandomLegalState(10, BoardSize.LARGE));
+//            System.out.println(Util.getRandomLegalState(10, BoardSize.LARGE));
+//            for (int i = 5; i < 8; i++) {
+//                String state = Util.getRandomLegalState(i, BoardSize.LARGE);
+//                Board board = Board.fromB64(state, BoardSize.LARGE);
+//                System.out.println("---------------------------------------   " + i + "   ---------------------------------------------------");
+//                System.out.println(board.toPrintString());
+//                System.out.println("\n\nrandom player");
+//                Move randMove = random.getNextMove(board);
+//
+//                System.out.println("\n\nuct player");
+//                Move uctMove = uct.getNextMove(board);
+//                System.out.println("!!!!     uct: " + uctMove + " rand; " + randMove + "    !!!!");
+//
+//            }
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-
-        Game game = new Game(board, p1, p2);
-        game.startGame();
-    }
-
-
-    @Test
-    void test() {
-        RandomMctsPlayer random = new RandomMctsPlayer(1000, true);
-        SteeredMctsPlayer uct = new SteeredMctsPlayer(1000, true);
-        try {
-            System.out.println(Util.getRandomLegalState(10, BoardSize.LARGE));
-            System.out.println(Util.getRandomLegalState(10, BoardSize.LARGE));
-            for (int i = 5; i < 8; i++) {
-                String state = Util.getRandomLegalState(i, BoardSize.LARGE);
-                Board board = Board.fromB64(state, BoardSize.LARGE);
-                System.out.println("---------------------------------------   " + i + "   ---------------------------------------------------");
-                System.out.println(board.toPrintString());
-                System.out.println("\n\nrandom player");
-                Move randMove = random.getNextMove(board);
-
-                System.out.println("\n\nuct player");
-                Move uctMove = uct.getNextMove(board);
-                System.out.println("!!!!     uct: " + uctMove + " rand; " + randMove + "    !!!!");
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+//
+//    }
 
 
 
